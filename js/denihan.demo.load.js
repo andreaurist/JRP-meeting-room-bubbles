660b69dc97b2b32bb7dace19c564b9ad605a47b7
andreaurist <andreaurist@yahoo.com>
2013-11-07 15:15:13 -0500

    Proof of concept: imagemap with popup bubbles
    uses jQuery plugin

---
+++ js/denihan.demo.load.js
@@ -0,0 +1,21 @@
+﻿jQuery(document).ready(function () {
+    // alert("HERE");
+
+    var bodyClass = jQuery("body").attr("class").toLowerCase();
+    var newLinkLocation = "";
+
+    if (bodyClass.indexOf("miami") > 0) {
+        newLinkLocation = "/miami";
+        jQuery("header h1#logo a").attr("href", newLinkLocation);
+    }
+    else if (bodyClass.indexOf("chicago") > 0) {
+        newLinkLocation = "/chicago";
+        jQuery("header h1#logo a").attr("href", newLinkLocation);
+    }
+    else if (bodyClass.indexOf("ny") > 0) {
+        newLinkLocation = "/new-york";
+        jQuery("header h1#logo a").attr("href", newLinkLocation);
+    }
+
+    jQuery("div.map_container span.point").denihanImageMap();
+});
---
+++ js/denihan.map.js
@@ -0,0 +1,26 @@
+﻿; (function ($) {
+    $.fn.denihanImageMap = function (options) {
+        // Create some defaults, extending them with any options that were provided
+        var settings = $.extend({
+            lorem: "things"
+        }, options);
+
+        function initialize($el) {
+            $el.denihanToggle({ target: $el.find("span.inside") });
+
+            var leftValue = "0";
+            var topValue = "0";
+
+            if($el.attr("data-denihan-point-x")) { leftValue = $el.attr("data-denihan-point-x"); }
+            if($el.attr("data-denihan-point-y")) { topValue = $el.attr("data-denihan-point-y"); }
+
+            $el.css("left", $el.attr("data-denihan-point-x") + "px");
+            $el.css("top", $el.attr("data-denihan-point-y") + "px");
+        }
+
+        return this.each(function () {
+            var $this = $(this);
+            initialize($this);
+        });
+    };
+})(jQuery);
